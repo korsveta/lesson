87bf8b2176952ab1e9389232f14b4c0f633f8901
Svetlana Koroleva <Lob.cve@yandex.ru>
2024-06-21 20:50:18 +0500

    lesson3

---
+++ lesson3.js
@@ -0,0 +1,23 @@
+// Задание №1
+const st='Привет Света';
+const num='22';
+const bool='true';
+const bd=null;
+const car=undefined;
+const ofv= {
+   boolean:true,
+   number:22,
+};
+const symb=Symbol('id');
+const big=BigInt(1000000);
+
+console.log('st', st, typeof st);
+console.log('num', num, typeof num);
+console.log('bool', bool, typeof bool);
+console.log('bd', bd, typeof bd);
+console.log('car', car, typeof car);
+console.log('ofv', ofv, typeof ofv);
+console.log('symb', symb, typeof symb);
+console.log('big', big, typeof big);
+
+
