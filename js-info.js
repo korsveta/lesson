2ce5db6c3784c537a1a6abc5fe614be68a14eaca
Svetlana Koroleva <Lob.cve@yandex.ru>
2024-06-21 17:22:50 +0500

    variable redefinition

--- js-info.js
+++ js-info.js
@@ -12,4 +12,11 @@ prompt("Какой сейчас год");
 alert("Ответ:2024");
 
 var text='Happy';
+let name='Sveta';
+const programming="JavaScript";
+
+name='Maxim';
+text='End';
+
 console.log(text);
+console.log(name);
